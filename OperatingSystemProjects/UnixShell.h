2065b84701d114f875f0893d19f31d531080c85f
Zhengning Li <zl132@georgetown.edu>
2015-11-19 23:26:52 -0500

    new progress

--- OperatingSystemProjects/UnixShell.h
+++ OperatingSystemProjects/UnixShell.h
@@ -58,3 +58,45 @@ typedef struct job {
 }t_job;
 
 static t_job* jobsList = NULL;
+
+static pid_t MSH_PID;
+static pid_t MSH_PGID;
+static int msh_TERMINAL, MSH_IS_INTERACTIVE;
+static struct termios MSH_TMODES;
+
+void pipelining(int);
+void getTextLine();
+
+void populateCommand();
+void destroyCommand();
+
+t_job * insertJob(pid_t , pid_t pgid, char* name, char* descriptor, int status);
+t_job * delJob(t_job* job);
+t_job * getJob(int searchValue, int searchParameter);
+
+void printJobs();
+
+void welcomeScreen();
+
+void shellPrompt();
+void handleUserCommand();
+
+int checkBuiltInCommand();
+
+void executeCommand(char *command[], char *file, int newDescriptor, int executionMode);
+
+void launchJob(char *command[], char *file, int newDescriptor, int executionMode);
+
+void putJobForeground(t_job* job, int continueJob);
+
+void putJobBackground(t_job* job, int continueJob);
+
+void waitJob(t_job* job);
+
+void killJob(int jobId);
+
+void changeDirectory();
+
+void init();
+
+void signalHandler_child(int p);
